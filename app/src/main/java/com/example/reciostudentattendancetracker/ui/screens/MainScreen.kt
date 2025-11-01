ba7b102a83256e0acd9d5156a2db57aee5fae5a9
Joronski <joronrecio@yahoo.com>
2025-11-01 11:38:28 +0800

    Modifying the Alignment of a headline as of 11:40 AM, 11/01/2025

--- app/src/main/java/com/example/reciostudentattendancetracker/ui/screens/MainScreen.kt
+++ app/src/main/java/com/example/reciostudentattendancetracker/ui/screens/MainScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 
 @Composable
@@ -67,7 +68,8 @@ fun MainScreen(
                         text = "Dangal Attendance Tracker",
                         style = MaterialTheme.typography.headlineLarge,
                         fontWeight = FontWeight.Bold,
-                        color = MaterialTheme.colorScheme.onPrimaryContainer
+                        color = MaterialTheme.colorScheme.onPrimaryContainer,
+                        textAlign = TextAlign.Center
                     )
                     Spacer(modifier = Modifier.height(8.dp))
                     Text(
